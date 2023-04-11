e75e858536a7353a58b5a173fdc6876dc72fa823
Biaksang Munsong <biaksangmunsong@gmail.com>
2023-04-11 16:47:06 +0530

    dev

--- socket-handlers/handleConnection.js
+++ socket-handlers/handleConnection.js
@@ -23,7 +23,7 @@ module.exports = async (io, socket) => {
 
     socket.on("set-lyrics-current-line", (line, songId) => {
         try {
-            socket.broadcast.emit("set-lyrics-current-line", line, songId)
+            io.emit("set-lyrics-current-line", line, songId)
         }
         catch {}
     })
