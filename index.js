260ac881c0411ee53e6e6cc05182f739d18cbe14
Biaksang Munsong <biaksangmunsong@gmail.com>
2023-04-11 17:29:06 +0530

    dev

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ app.use((err, req, res, next) => {
     }
 })
 
+// Handle 404 errors
+app.use((req, res, next) => {
+    res
+    .status(404)
+    .set("Cache-Control", "no-store")
+    .sendFile(__dirname + "/public/index.html")
+})
+
 // listen to port
 server.listen(PORT, () => {
     console.log(`Server running at port ${PORT}...`)
